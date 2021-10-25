f98c385da0e2578f58d857290cc158c26f2d9710
Lihacovs <k.lihacovs@inbox.lv>
2021-10-25 17:20:36 +0100

    #2

--- app/src/main/java/eu/balticit/copyrightly/data/firebase/model/User.kt
+++ app/src/main/java/eu/balticit/copyrightly/data/firebase/model/User.kt
@@ -1,4 +1,14 @@
 package eu.balticit.copyrightly.data.firebase.model
 
-class User {
-}
+data class User(
+    val userId : String,
+    val userEmail : String,
+    val userPassword : String,
+    val userName : String,
+    val userPhotoUrl : String,
+    val userGender : String,
+    val userBirthDate : String,
+    val userCreationDate : String,
+    val userPremium : Boolean,
+    val userAdmin : Boolean
+)
